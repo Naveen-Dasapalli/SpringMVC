04d7fb06f1e241e94efec96c32f0df330cf6efc6
Naveen-Dasapalli <naveendasapalli@outlook.com>
2024-11-30 19:01:42 +0530

    added new UserManagament Project

---
+++ SpringMVC_UserManagementRESTapi/src/main/java/com/UserManagementRESTapi/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.UserManagementRESTapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+
+}
